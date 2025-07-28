e26f8ba4ddea3b4b65ccabb6f624e2c36a6c9d51
Webb <webbcody04@gmail.com>
2025-07-28 15:30:43 -0600

    Footer component changes

--- my-app/components/Footer.tsx
+++ my-app/components/Footer.tsx
@@ -1,10 +1,27 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 py-6 border-t border-gray-700">
-      <div className="max-w-7xl mx-auto px-8 text-center text-sm select-none">
-        <p>Internet Movies Rental Company (IMR)</p>
-        <p>© {new Date().getFullYear()}</p>
+      <div className="max-w-7xl mx-auto px-8 text-center text-sm select-none space-y-2">
+        <p className="font-semibold">Internet Movies Rental Company (IMR)</p>
+        <p>© {new Date().getFullYear()} IMR, Inc. All rights reserved.</p>
+        <p>1234 Movie Lane, Suite 100, Film City, CA 90001</p>
+        <p>Email: support@imrmovies.com | Phone: (999) 123-4567</p>
+        <p>
+          Follow us on{' '}
+          <a href="#" className="underline hover:text-white">
+            Twitter
+          </a>{' '}
+          |{' '}
+          <a href="#" className="underline hover:text-white">
+            Facebook
+          </a>{' '}
+          |{' '}
+          <a href="#" className="underline hover:text-white">
+            Instagram
+          </a>
+        </p>
       </div>
     </footer>
   );
 }
+
